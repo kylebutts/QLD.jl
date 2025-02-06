06303a71594f6f54167e4494d1da3bf90cdbcafc
Kyle Butts <buttskyle96@gmail.com>
2025-02-06 07:33:54 -0600

    Fix slightly off standard error calculations

--- src/QLD.jl
+++ src/QLD.jl
@@ -1,27 +1,8 @@
-module QLD
 using LinearAlgebra
 using Statistics
 using StatsBase
-import Distributions
+using Distributions: Distributions
 using Optim
-import ForwardDiff
-import LineSearches
-import DataFrames
-
-include("qld_imputation.jl")
-include("gmm_qld.jl")
-include("attgt.jl")
-include("within_transform.jl")
-
-# "GMM routine to estimate QLD"
-export gmm_qld
-
-
-# "Brown and Butts (2024) Within Transformation"
-export within_transform
-
-# "Brown and Butts (2024) QLD Imputation Routine"
-export qld_imputation
-
-end # module QLD
-
+using ForwardDiff: ForwardDiff
+using LineSearches: LineSearches
+using DataFrames: DataFrames
